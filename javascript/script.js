4b27a35259eafd515440ddb4958f9ac020ca4481
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-02-01 19:19:17 +0100

    Added basic logic for updating screen
    Created a div+class for a group
    Added an event listen. to the groupe to capture click.
    Created an if-else structure to assign value to
    the first operand, operator and last operand

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 // Basic maths functions
 // Your calculator is going to contain functions for all of the basic math operators you typically find on calculators,
 // so start by creating functions for the following items and testing them in your browser’s console. 
-const calculator = {
+const mathFunctions = {
     add: (a,b) => a+b,
     substract: (a,b) => a-b,
     divide: (a,b) => a/b,
@@ -10,12 +10,49 @@ const calculator = {
 // A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
 // Create three variables for each of the parts of a calculator operation.
 // Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
-let firstOperand = 2
-let operator = calculator.multiply
-let lastOperand = 2
+let firstOperand="";
+let operator="";
+let lastOperand ="";
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
-const operate = (a,b,c) => {
+const calculateResult = (a,b,c) => {
     return b(a,c)
 }
-console.log(operate(firstOperand, operator, lastOperand))
+
+//Link to DOM
+const calculatorButtons = document.querySelector(".calculator-buttons")
+const displayMainScreen = document.querySelector("#main-screen")
+
+
+// Functions to clear and update screen
+updateMainScreen = () => {
+    displayMainScreen.textContent=firstOperand+operator+lastOperand;
+}
+
+clearAll = () => {
+    firstOperand="";
+    operator="";
+    lastOperand="";
+    updateMainScreen();
+}
+
+// Each time a button is clicked
+calculatorButtons.addEventListener("click", (event) => {
+    if (event.target.classList.contains("num")) {
+        if (operator===""){
+        firstOperand += event.target.id.toString();
+        updateMainScreen();
+        } else if (operator!==""){
+        lastOperand += event.target.id.toString();
+        updateMainScreen();
+        } 
+    } else if (event.target.classList.contains("operator") && firstOperand!==""){
+        operator = event.target.id.toString();
+        updateMainScreen();
+    } else if (event.target.id === "clear-btn") {
+        clearAll();
+    }
+})
+// EVerytime a num button is clicked firstOperand+= id value.
+// If an operator class is clicked add to operator.
+// If an operator is present the num button is stored in last operand.
